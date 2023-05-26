5d4d7d6e41cac34fd05ab9ea689d18252fb81098
ma7moud3zim <eng3zim@gmail.com>
2023-05-26 06:23:46 +0300

    DesignedAllActivities
    I designed all activites and start working on logic.

--- app/src/main/java/com/example/abrag/MainActivity.kt
+++ app/src/main/java/com/example/abrag/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View.OnClickListener
 import android.widget.Button
+import com.example.abrag.convert.FadanToMeter
 import com.example.abrag.rectangle.AreaDiameter
 import com.example.abrag.rectangle.AreaWidthHeight
 import com.example.abrag.triangle.Area3Lines
@@ -23,34 +24,40 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        // Click listener go to activity of calculating area with 3 lines
+        // Click listener go to activity of calculating area of triangle with 3 angles
         val btn3ang:Button = findViewById(R.id.btn_3ang)
         btn3ang.setOnClickListener {
             val intent = Intent(this@MainActivity, Area3angles::class.java)
             startActivity(intent)
         }
 
-        // Click listener go to activity of calculating area with 3 lines
+        // Click listener go to activity of calculating area of triangle with height and base
         val btnHBase:Button = findViewById(R.id.btn_hbase)
         btnHBase.setOnClickListener {
             val intent = Intent(this@MainActivity, AreaHeightBase::class.java)
             startActivity(intent)
         }
 
-        // Click listener go to activity of calculating area with 3 lines
+        // Click listener go to activity of calculating area of rectangle with width and height
         val btnRectAreaWidthHeight:Button = findViewById(R.id.btn_rect_width_height)
         btnRectAreaWidthHeight.setOnClickListener {
             val intent = Intent(this@MainActivity, AreaWidthHeight::class.java)
             startActivity(intent)
         }
 
-        // Click listener go to activity of calculating area with 3 lines
+        // Click listener go to activity of calculating area with diameter
         val btnRectDiameter:Button = findViewById(R.id.btn_diameter)
         btnRectDiameter.setOnClickListener {
             val intent = Intent(this@MainActivity, AreaDiameter::class.java)
             startActivity(intent)
         }
 
+        // Click listener go to activity that convert fadan to meter²
+        val btnFadanToMeter:Button = findViewById(R.id.fadan_to_meter)
+        btnFadanToMeter.setOnClickListener {
+            val intent = Intent(this@MainActivity, FadanToMeter::class.java)
+            startActivity(intent)
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/abrag/convert/FadanToMeter.kt
@@ -0,0 +1,12 @@
+package com.example.abrag.convert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.abrag.R
+
+class FadanToMeter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fadan_to_meter)
+    }
+}
