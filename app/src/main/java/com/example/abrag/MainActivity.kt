7c6a9c26694bdd836ce7963d7349910fd3337ba7
ma7moud3zim <eng3zim@gmail.com>
2023-05-26 05:53:20 +0300

    DesignRectangle
    Design the activities of Rectangle. and handle the click listeners for every activity.

--- app/src/main/java/com/example/abrag/MainActivity.kt
+++ app/src/main/java/com/example/abrag/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View.OnClickListener
 import android.widget.Button
+import com.example.abrag.rectangle.AreaDiameter
+import com.example.abrag.rectangle.AreaWidthHeight
 import com.example.abrag.triangle.Area3Lines
 import com.example.abrag.triangle.Area3angles
 import com.example.abrag.triangle.AreaHeightBase
@@ -28,10 +30,24 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-                // Click listener go to activity of calculating area with 3 lines
+        // Click listener go to activity of calculating area with 3 lines
         val btnHBase:Button = findViewById(R.id.btn_hbase)
         btnHBase.setOnClickListener {
             val intent = Intent(this@MainActivity, AreaHeightBase::class.java)
+            startActivity(intent)
+        }
+
+        // Click listener go to activity of calculating area with 3 lines
+        val btnRectAreaWidthHeight:Button = findViewById(R.id.btn_rect_width_height)
+        btnRectAreaWidthHeight.setOnClickListener {
+            val intent = Intent(this@MainActivity, AreaWidthHeight::class.java)
+            startActivity(intent)
+        }
+
+        // Click listener go to activity of calculating area with 3 lines
+        val btnRectDiameter:Button = findViewById(R.id.btn_diameter)
+        btnRectDiameter.setOnClickListener {
+            val intent = Intent(this@MainActivity, AreaDiameter::class.java)
             startActivity(intent)
         }
 
---
+++ app/src/main/java/com/example/abrag/rectangle/AreaWidthHeight.kt
@@ -0,0 +1,12 @@
+package com.example.abrag.rectangle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.abrag.R
+
+class AreaWidthHeight : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rect_width_height)
+    }
+}
