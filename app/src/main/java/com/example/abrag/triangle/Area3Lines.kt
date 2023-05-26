18faa0eb246fd8b9397a565dc0f7abfecd980e56
ma7moud3zim <eng3zim@gmail.com>
2023-05-26 05:15:00 +0300

    TriangleActivities
    Add the activities of triangle tools and design the first activity of calculating the area knowing three lines of it.

---
+++ app/src/main/java/com/example/abrag/triangle/Area3Lines.kt
@@ -0,0 +1,12 @@
+package com.example.abrag.triangle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.abrag.R
+
+class Area3Lines : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_area3lines)
+    }
+}
---
+++ app/src/main/java/com/example/abrag/triangle/Area3angles.kt
@@ -0,0 +1,12 @@
+package com.example.abrag.triangle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.abrag.R
+
+class Area3angles : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_area3angles)
+    }
+}
